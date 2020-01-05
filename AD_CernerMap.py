22d119601e5456608e1be6b082dc80f0e0105812
gordont <gordont.@monhealthsys.org>
2020-01-05 09:50:46 -0500

    created LDAP connector, check_username, create_username, list_usernames methods

--- AD_CernerMap.py
+++ AD_CernerMap.py
@@ -1,29 +1,64 @@
 ### AD Cerner Map ###
 
-import csv
 from getpass import getpass
 import ldap
 import pprint
+from PARSER import list_column, split_list
+from typing import List
 
-## Pretty Print ##
+## Configs ##
+
+# Pretty Print #
 pp = pprint.PrettyPrinter(indent=1)
 
+# SpreadSheet #
+path = 'Providers.xlsx'
+
 ## Prompt ##
-#user = input('Username: ')
 user = 'gordont@monhealthsys.org'
 pssw = getpass()
 
-## LDAP INIT ##
+## LDAP Configs ##
 conn = ldap.initialize('ldap://172.26.5.141')
 conn.protocol_version = 3
 conn.set_option(ldap.OPT_REFERRALS, 0)
 conn.simple_bind_s(user,pssw)
 
-## Search ##
-response = conn.search_s(
-    'dc=mhs,dc=org', ldap.SCOPE_SUBTREE,
-    'userPrincipalName=abbasz@monhealthsys.org')
-pp.pprint(response)
+## Create Usernames ## 
+## Query User ##
+## TODO ##
+# 
+# 
+def check_username(uname: str) -> bool:
+  sam = 'sAMAccountName=' + uname
+  response = conn.search_s('dc=mhs,dc=org', ldap.SCOPE_SUBTREE, sam)
+  answer = str([tuple[0] for tuple in response][0])
+  # If answer is None return False
+  if answer == 'None':
+    return False
+  return True
 
+def create_username(name: List[str], lost: List[str]) -> str:
+  rubric    = ['Last','First','Mid']
+  named = {k:v for k,v in zip(rubric,name)}
+  if len(named['First']) == 1:
+    pocket = named['First']
+    named['First'] = named['Mid']
+    named['Mid'] = pocket
+  username  = named['Last'] + named['First'][0]
+  # If username does not exist
+  if not check_username(username):
+     lost.append(username)
+  return username
 
+def list_usernames(path: str, col: str) -> List[str]:
+  full_names = list_column(path,col)
+  names = [name for name in split_list(full_names) if name]
+  lost_users = []
+  mill_users = [create_username(name, lost_users) for name in names]
+  #users = [list(filter(None,users)) for name in users]
+  return mill_users, lost_users
 
+mill_users, lost_users = list_usernames(path,'B')
+pp.pprint(mill_users)
+pp.pprint(lost_users)
--- PARSER.py
+++ PARSER.py
@@ -1,10 +1,10 @@
-## Parse XLSX ##
+## XLSX PARSER ##
 from bisect import insort
 from openpyxl import load_workbook
 from os import path
 from typing import List, Dict, Iterable
 import re
-import pprint 
+import pprint
 
 # Set PPrint
 pp = pprint.PrettyPrinter(indent=1)
@@ -13,7 +13,7 @@ pp = pprint.PrettyPrinter(indent=1)
 path = 'Providers.xlsx'
 
 def list_column(spreadsheet: str, column: str) -> List[str]:
-  ## Takes a path to an xlsx and a column and returns a list 
+  ## Takes a path to an xlsx and a column and returns a list
   ## TODO ##
   # Path should be more than a str, meaning check OS and then use appropriate
   # function to check if path exists
@@ -28,14 +28,14 @@ def normalize_list(names: List[str]) -> List[str]:
   de_s  = "(),_."
   clean = str.maketrans(dict.fromkeys(de_s))
 
-  # Remove tokens that can't be removed absolutely 
+  # Remove tokens that can't be removed absolutely
   normalized: List[str] = sorted([re.sub(
-    r'(Test\s.*)|(Cerner)|(Physician)|(Advisor)|(\sHw)|(De La)|(^St\s)|[A-Z]{2,}|\b[A-Z]?-\S+|(\s)(?=\s)',
+    r'(Test\s.*)|(Cerner)|(Physician)|(Advisor)|(\sHw)|(De La)|(O\')|(^St\s)|[A-Z]{2,}|\b[A-Z]?-\S+|(\s)(?=\s)',
     # Remove any Names containing Test
     # Remove any (<WORD>)
     # [A-Z]{2,}     : Remove any al caps words of two or more chars (i.e., MD,PHD,etc)
     # \b[A-Z]?-\S+  : Remove '-' and any following word
-    # (\s)(?=\s)    : Remove double whitespaces 
+    # (\s)(?=\s)    : Remove double whitespaces
     '',
     str(n.value).translate(clean)).strip()
     for n in names
@@ -46,23 +46,21 @@ def split_list(names: List[str]) -> List[List[str]]:
   # Takes a list of names and returns a list of each name in a full name
   # If no middle name is given, create a NULL value
   normal = normalize_list(names[1:])
-  
   split_names = list(
-      sub.append('NULL') or sub if len(sub) == 2 
-      else sub 
+      sub.append('NULL') or sub if len(sub) == 2
+      else sub
       for sub in list(re.split(r'\s', name) for name in normal))
-  
   split_names = [list(filter(None,name)) for name in split_names]
   return split_names
 
 def create_username(name: List[str]) -> str:
   rubric    = ['Last','First','Mid']
-  user_dict = {k:v for k,v in zip(rubric,name)}
-  if not len(user_dict['First']) > 1:
-    pocket = user_dict['First']
-    user_dict['First'] = user_dict['Mid']
-    user_dict['Mid'] = pocket 
-  username  = user_dict['Last']+user_dict['First'][0]
+  named = {k:v for k,v in zip(rubric,name)}
+  if len(named['First']) == 1:
+    pocket = named['First']
+    named['First'] = named['Mid']
+    named['Mid'] = pocket
+  username  = named['Last']+named['First'][0]
   return username
 
 def list_usernames(path: str, col: str) -> List[str]:
@@ -73,5 +71,3 @@ def list_usernames(path: str, col: str) -> List[str]:
     users.append(create_username(name))
   return users
 
-answer = list_usernames(path,'B')
-print(len(answer))
